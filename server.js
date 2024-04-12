983d0b9abfc508eabf82e4b9c9819415c148b19a
Elyas Fahmi Ahmed Abdullah <elyasabdullah771124@gmail.com>
2024-04-12 16:13:39 +0300

    Deleting the package-lock.json from the frontend folder and modifying the server.js file to server the frontend files

--- server.js
+++ server.js
@@ -40,16 +40,12 @@ app.use(verifyJWT);
 app.use('/activities', require('./routes/api/activities'))
 app.use('/users', require('./routes/api/users'));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
+
 
 app.use(errorHandler);
 
